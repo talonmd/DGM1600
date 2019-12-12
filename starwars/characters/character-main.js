9a5b2584eb6465bc677ca53b43667aedba33ddec
Talon Durrant <talonmdurrant@gmail.com>
2019-12-12 12:51:23 -0700

    added characters assignment to portfolio

--- starwars/characters/character-main.js
+++ starwars/characters/character-main.js
@@ -1,35 +1,6 @@
 import { people } from '../assets/people.js'
 
-// main navigation below
-
 let mainSection = document.querySelector('main')
-let mainHeader = document.querySelector('header')
-
-let crawlButton = document.createElement('button')
-let planetButton = document.createElement('button')
-let characterButton = document.createElement('button')
-
-crawlButton.textContent = 'View Each Films Opening Crawl'
-planetButton.textContent = 'Check out the planets of the Star Wars Universe'
-characterButton.textContent = 'See the characters of the Star Wars Universe'
-
-mainHeader.appendChild(crawlButton)
-mainHeader.appendChild(planetButton)
-mainHeader.appendChild(characterButton)
-
-characterButton.setAttribute('class', 'active')
-
-crawlButton.addEventListener('click', () => {
-    window.location = "../crawl/crawl.html"
-})
-
-planetButton.addEventListener('click', () => {
-    window.location = "../planets/planets.html"
-})
-
-characterButton.addEventListener('click', () => {
-    window.location = "../characters/characters.html"
-})
 
 // filter buttons below
 
